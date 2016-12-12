7773a97fb42051a5510680b6515a8b273ac3fb4b
Jason R. Carrete <jasoncarrete5@gmail.com>
2016-12-12 16:04:58 -0500

    Set up leaf nodes of huffman tree

--- huffman.py
+++ huffman.py
@@ -1,6 +1,32 @@
-from Queue import PriorityQueue
+#
+# huffman.py
+# Demonstartes Huffman coding and decoding
+# Created by: Jason R. Carrete
+# Written with Python 3
+#
 
-class BiNode:
-	def __init__(self):
-		self.symbol = None
-		self.weight = 0
+from queue import PriorityQueue
+
+class Node:
+	def __init__(self, symbol, weight):
+		self.parent = None
+		self.left = None
+		self.right = None
+		self.symbol = symbol
+		self.weight = weight
+
+	def __lt__(self, other):
+		return self.weight < other.weight
+
+infile = open("input.txt", "r")
+data = infile.read()
+infile.close()
+
+pQueue = PriorityQueue()
+for ch in set(data):
+	node = Node(ch, data.count(ch) / len(data))
+	pQueue.put(node)
+while not pQueue.empty():
+	sym = pQueue.get().symbol
+	print("********")
+	print(sym + " : " + str(ord(sym)))
