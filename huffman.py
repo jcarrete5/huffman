6f4dbdd9404244cdf1255c0db0e731b5d9c97cea
Jason Carrete <jasoncarrete5@gmail.com>
2016-12-12 23:07:45 -0500

    Successfully constructed the prefix-tree
    Printed the tree in level order traversal when complete

--- huffman.py
+++ huffman.py
@@ -6,6 +6,7 @@
 #
 
 from queue import PriorityQueue
+from queue import Queue
 
 class Node:
 	def __init__(self, symbol, weight):
@@ -18,15 +19,39 @@ class Node:
 	def __lt__(self, other):
 		return self.weight < other.weight
 
-infile = open("input.txt", "r")
-data = infile.read()
-infile.close()
+	def __str__(self):
+		return "ASCII: " + str(ord(self.symbol)) + "\nWeight: " + str(self.weight)
+
+with open("input.txt", "r") as infile:
+	data = infile.read()
 
 pQueue = PriorityQueue()
 for ch in set(data):
-	node = Node(ch, data.count(ch) / len(data))
+	node = Node(ch, data.count(ch))
 	pQueue.put(node)
-while not pQueue.empty():
-	sym = pQueue.get().symbol
-	print("********")
-	print(sym + " : " + str(ord(sym)))
+
+while pQueue.qsize() > 1:
+	n1 = pQueue.get()
+	n2 = pQueue.get()
+
+	internal = Node('\0', n1.weight + n2.weight)
+	n1.parent = internal
+	n2.parent = internal
+	internal.left = n1
+	internal.right = n2
+
+	pQueue.put(internal)
+
+root = pQueue.get()
+
+buf = Queue()
+buf.put(root)
+# traverse the binary tree using Level Order Traversal
+while not buf.empty():
+	node = buf.get()
+	print(str(node) + "\n")
+
+	if node.left is not None:
+		buf.put(node.left)
+	if node.right is not None:
+		buf.put(node.right)
