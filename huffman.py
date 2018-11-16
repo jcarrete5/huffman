fa07ec7f1192ab4ad8cf86497b5c4655c01e0176
Jason Carrete <jasoncarrete5@gmail.com>
2018-11-15 21:09:26 -0500

    Added shebang and reorganized some code

--- huffman.py
+++ huffman.py
@@ -1,46 +1,46 @@
+#!/usr/bin/env python3
 #
 # huffman.py
 # Demonstartes Huffman coding and decoding
 # Created by: Jason R. Carrete
-# Written with Python 3
 #
 
 from queue import PriorityQueue
 from queue import Queue
 
 class Node:
-	def __init__(self, symbol, weight):
-		self.parent = None
-		self.left = None
-		self.right = None
-		self.symbol = symbol
-		self.weight = weight
+    def __init__(self, symbol, weight):
+        self.parent = None
+        self.left = None
+        self.right = None
+        self.symbol = symbol
+        self.weight = weight
 
-	def __lt__(self, other):
-		return self.weight < other.weight
+    def __lt__(self, other):
+        return self.weight < other.weight
 
-	def __str__(self):
-		return "ASCII: " + str(ord(self.symbol)) + "\nWeight: " + str(self.weight)
+    def __str__(self):
+        return "{}\t-> {}".format(hex(ord(self.symbol)), self.weight)
 
 with open("input.txt", "r") as infile:
-	data = infile.read()
+    data = infile.read()
 
 pQueue = PriorityQueue()
 for ch in set(data):
-	node = Node(ch, data.count(ch))
-	pQueue.put(node)
+    node = Node(ch, data.count(ch))
+    pQueue.put(node)
 
 while pQueue.qsize() > 1:
-	n1 = pQueue.get()
-	n2 = pQueue.get()
+    n1 = pQueue.get()
+    n2 = pQueue.get()
 
-	internal = Node('\0', n1.weight + n2.weight)
-	n1.parent = internal
-	n2.parent = internal
-	internal.left = n1
-	internal.right = n2
+    internal = Node('\0', n1.weight + n2.weight)
+    n1.parent = internal
+    n2.parent = internal
+    internal.left = n1
+    internal.right = n2
 
-	pQueue.put(internal)
+    pQueue.put(internal)
 
 root = pQueue.get()
 
@@ -48,10 +48,11 @@ buf = Queue()
 buf.put(root)
 # traverse the binary tree using Level Order Traversal
 while not buf.empty():
-	node = buf.get()
-	print(str(node) + "\n")
+    node = buf.get()
+    print(str(node))
 
-	if node.left is not None:
-		buf.put(node.left)
-	if node.right is not None:
-		buf.put(node.right)
+    if node.left is not None:
+        buf.put(node.left)
+    if node.right is not None:
+        buf.put(node.right)
+
